df60373a51b2dd45e50154634e42ce680712917f
Otus Student <vvscode+otus-js-student@gmail.com>
2020-10-05 21:45:49 +0300

    Setup tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: ["airbnb-base", "plugin:jest/recommended", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
---
+++ src/testFunction.js
@@ -0,0 +1,3 @@
+export default function testFunction() {
+  return "Hello world";
+}
