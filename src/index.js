4193b0bc76ebf119791ebdffd0df886fa018d085
Ek. Tereshko <Ek.Tereshko@gmail.com>
2020-10-19 21:13:43 +0300

    Add demo example

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@
 //
 // - создать элемент и добавить его на страницу
 // - создать на этом элементе игру с помощью `createGameOfLife` с размерами поля x / y
+
+const { createGameOfLife } = require("./createGameOfLife");
+
 // - для проверки своего кода можно создать еще один элемент и создать вторую игру на этой же странице
+const gameWrapper1 = document.createElement("div");
+const gameWrapper2 = document.createElement("div");
+
+document.body.appendChild(gameWrapper1);
+document.body.appendChild(gameWrapper2);
+
+createGameOfLife(3, 3, gameWrapper1);
+createGameOfLife(10, 10, gameWrapper2);
