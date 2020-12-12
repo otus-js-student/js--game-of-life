0cc807931e48475e68d2dc43bcb48b441673de84
Vasil Vanchuk <v.vanchuk@tut.by>
2020-12-12 13:34:37 +0200

    Show multiple games on the screen

--- src/index.js
+++ src/index.js
@@ -8,11 +8,8 @@ import "./styles.css";
 const { createGameOfLife } = require("./createGameOfLife");
 
 // - для проверки своего кода можно создать еще один элемент и создать вторую игру на этой же странице
-const gameWrapper1 = document.createElement("div");
-const gameWrapper2 = document.createElement("div");
-
-document.body.appendChild(gameWrapper1);
-document.body.appendChild(gameWrapper2);
-
-createGameOfLife(3, 3, gameWrapper1);
-createGameOfLife(10, 10, gameWrapper2);
+for (let i = 0; i <= 10; i += 1) {
+  const gameWrapper = document.createElement("div");
+  document.body.appendChild(gameWrapper);
+  createGameOfLife(3 + i, 3 + i, gameWrapper);
+}
